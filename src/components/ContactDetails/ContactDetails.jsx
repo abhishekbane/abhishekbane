6d68da8850d70bcbcf0a6ddf545361e1949eb41a
Abhishek Bane <abhishek.bane97@gmail.com>
2021-01-17 22:47:15 +0530

    Refined contact details

--- src/components/ContactDetails/ContactDetails.jsx
+++ src/components/ContactDetails/ContactDetails.jsx
@@ -5,17 +5,17 @@ import Section from '../UI/Section/Section';
 import styles from './ContactDetails.module.css';
 import { CONTACT_DETAILS } from '../../containers/AskQuestions/QuestionsData';
 import {paragraph as Paragraph } from '../UI/Paragraphs/Paragraphs';
+import { hyperlinkYellow as Hyperlink } from '../UI/Hyperlink/Hyperlink';
 
 const contactDetails = () => {
     return (
         <Section heading="Contact Details" anchorId = { CONTACT_DETAILS }>
             <address className={ styles.address }>
-                <Paragraph margin="0" fontSize="1rem">Name: Abhishek Bane</Paragraph>
-                <Paragraph margin="0" fontSize="1rem">
-                    Email: <a href="mailto:abhishek.bane97@gmail.com">abhishek.bane97@gmail.com</a>
+                <Paragraph margin="1rem 1.5rem 0.5rem 1.5rem" fontSize="1rem">
+                    Name: <Hyperlink href="https://www.linkedin.com/in/abhishek-bane-012589b9/">Abhishek Bane</Hyperlink>
                 </Paragraph>
-                <Paragraph margin="0" fontSize="1rem">
-                    Linkedin: <a href="https://www.linkedin.com/in/abhishek-bane-012589b9/">linkedin.com/in/abhishek-bane-012589b9</a>
+                <Paragraph margin="0.5rem 1.5rem 1.5rem 1.5rem" fontSize="1rem">
+                    Email: <Hyperlink href="mailto:abhishek.bane97@gmail.com">abhishek.bane97@gmail.com</Hyperlink>
                 </Paragraph>
             </address>
         </Section>
